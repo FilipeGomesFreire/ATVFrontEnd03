2a3a1aa195d4ee1e325e9a1c0fe1a31478c3067d
FilipeTrap <trap_krisiun@hotmail.com>
2024-09-07 00:37:36 -0300

    finalizado

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -37,6 +37,9 @@ const pauseAudio = () => {
 
     document.body.style.backgroundImage = "url('https://img1.picmix.com/output/stamp/normal/2/4/9/1/1661942_cbf06.gif')";
 
+    //document.getElementById('minhaImagem').style.visibility = 'visible';
+
+
     pauseAudio();
 
     
@@ -51,7 +54,7 @@ const pauseAudio = () => {
     const newNumber2 = Math.floor(Math.random() * 6) + 1;
     setNumber2(newNumber2);
 
-      alterarimagens(newNumber1, newNumber2)
+      alterarimagens(newNumber1, newNumber2, contadorDerrotas)
       verificar(newNumber1,newNumber2);
     }, 800)
 
@@ -59,7 +62,8 @@ const pauseAudio = () => {
     
   }
 
-  function alterarimagens(newNumber1, newNumber2){
+  function alterarimagens(newNumber1, newNumber2, contadorDerrotas){
+    console.log("alterar imagem");
     if(newNumber1 === 1){
       setImage1('https://i.imgur.com/KHcK0tk.png');
     }
@@ -97,10 +101,46 @@ const pauseAudio = () => {
     if(newNumber2 === 6){
       setImage2('https://i.imgur.com/3X2Jmi6.png');
     }
+    //----------------------------------------------------
+   
+    if (contadorDerrotas >= 2 && contadorDerrotas < 5) {
+      setIncrivel('https://i.pinimg.com/236x/81/9d/3d/819d3dce0441f53fa7232dcbc0c40fdf.jpg');
+    } 
+    if (contadorDerrotas >= 5 && contadorDerrotas < 7) {
+      setIncrivel('https://i.pinimg.com/236x/fb/29/56/fb2956e925ef817e82203897a69e699a.jpg');
+    }
+    if (contadorDerrotas >= 7 && contadorDerrotas < 10) {
+      setIncrivel('https://i.pinimg.com/236x/0d/61/11/0d6111abed9cd51b09294fd27ff56be6.jpg');
+    }
+    if (contadorDerrotas >=10 && contadorDerrotas < 12) {
+      setIncrivel('https://i.pinimg.com/236x/ff/2e/02/ff2e0218be0890572cc13596a7e45cbb.jpg');
+    }
+    
+    if (contadorDerrotas >=12 && contadorDerrotas < 15) {
+      setIncrivel('https://i.pinimg.com/236x/8d/b6/bb/8db6bb1d597920100c58a2d70c864fd3.jpg');
+    }
+    
+    if (contadorDerrotas >=15 && contadorDerrotas <18 ) {
+      setIncrivel('https://i.pinimg.com/236x/96/f1/fe/96f1fe857218bc99540af8a2f51cdbfc.jpg');
+    }
+    
+    if (contadorDerrotas >=18 && contadorDerrotas <20 ) {
+      setIncrivel('https://i.pinimg.com/236x/e4/dc/a8/e4dca847978d7224b53855a33de7a226.jpg');
+    }
+    
+    if (contadorDerrotas >=20) {
+      setIncrivel('https://i.pinimg.com/236x/8b/b6/24/8bb624e42a9fdc9bffc6f98ee74439b3.jpg');
+    }
+    
+
   }
 
   function verificar(num1, num2){
     if(num1 + num2 === 11 || num1 + num2 === 7 ){
+
+      //document.getElementById('minhaImagem').style.visibility = 'hidden';
+      setIncrivel('https://i.pinimg.com/236x/ec/e9/fb/ece9fbaff26d460b5abafbf6d1bdd40f.jpg');
+
   
       // const newcontadorVitorias = contadorVitorias+1; //assim ta q nem o exemplo em sala, só aparece depois q atualiza 
       // setContadorVitorias(newcontadorVitorias);
@@ -133,37 +173,7 @@ const pauseAudio = () => {
       return "Nenhuma jogada realizada";
     }
 
-    // if (contadorDerrotas < 2) {
-    //   setIncrivel('https://i.imgur.com/KHcK0tk.png');
-    // } else if (contadorDerrotas >= 2 && contadorDerrotas < 5) {
-    //   setIncrivel('https://i.pinimg.com/236x/81/9d/3d/819d3dce0441f53fa7232dcbc0c40fdf.jpg');
-    // } else if (contadorDerrotas >= 5 && contadorDerrotas < 7) {
-    //   setIncrivel('https://i.pinimg.com/236x/fb/29/56/fb2956e925ef817e82203897a69e699a.jpg');
-    // }
 
-    /*
-    imagens em sequencia
-
-    boas
-    https://i.pinimg.com/236x/ec/e9/fb/ece9fbaff26d460b5abafbf6d1bdd40f.jpg
-    https://i.pinimg.com/236x/5c/97/52/5c9752e01b907bb655dfa043c4faf6dc.jpg
-    
-    neutro 
-    https://i.pinimg.com/236x/ae/2f/2d/ae2f2d3be50269e19ebb2d53c1c4b3f9.jpg
-    https://i.pinimg.com/236x/81/9d/3d/819d3dce0441f53fa7232dcbc0c40fdf.jpg
-
-    ruins 
-    https://i.pinimg.com/236x/fb/29/56/fb2956e925ef817e82203897a69e699a.jpg
-    https://i.pinimg.com/236x/0d/61/11/0d6111abed9cd51b09294fd27ff56be6.jpg
-    https://i.pinimg.com/236x/ff/2e/02/ff2e0218be0890572cc13596a7e45cbb.jpg
-    https://i.pinimg.com/236x/8d/b6/bb/8db6bb1d597920100c58a2d70c864fd3.jpg
-    https://i.pinimg.com/236x/96/f1/fe/96f1fe857218bc99540af8a2f51cdbfc.jpg
-    https://i.pinimg.com/236x/e4/dc/a8/e4dca847978d7224b53855a33de7a226.jpg
-    https://i.pinimg.com/236x/8b/b6/24/8bb624e42a9fdc9bffc6f98ee74439b3.jpg
-
-
-    
-    */
   
     const porcentagemVitorias = (contadorVitorias / totalJogadas) * 100;
     
@@ -175,7 +185,9 @@ const pauseAudio = () => {
   return (
     <div>
       
-      <img id='imgIncrivel' src={incrivel} width="200" />
+    
+      <img id='imgIncrivel' src={incrivel} width="200" alt="" />
+
       <br/>
       <div id='dados'>
       <h1>JOGO DO TIGRINHO!!!!!</h1>
@@ -194,7 +206,7 @@ const pauseAudio = () => {
       
 
       
-      <div style={{visibility: 'visible', marginTop: '20px', fontSize: '24px' }}>
+      <div style={{visibility: 'hidden', marginTop: '20px', fontSize: '24px' }}>
         dado 1: {number1}
         dado 2: {number2}
         vitorias: {contadorVitorias}
